3e11941153cdadef6303bf85564a286bd275128e
Tom Luijten <tbmluijten@gmail.com>
2021-01-26 12:44:13 +0100

    [ADD] Week 1 project

---
+++ week-1/src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <nav>
+      <div class="container">
+        <div class="holder-navigation">
+            <router-link  class="link" to="/">Home</router-link>
+            <router-link  class="link" to="/about">About</router-link>
+        </div>
+      </div>
+        
+      
+    </nav>
+    
+    <router-view/>
+
+    <footer>
+      <div class="container">
+        <div class="holder-details">
+          <div class="title">Vue training 2021</div>
+          <div class="name">Tom Luijten</div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;500&display=swap');
+  body{
+    margin: 0;
+    padding-bottom: 103px;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  page{
+    min-height: 100vh;
+  }
+
+  .container{
+    max-width: 1200px;
+    padding: 0px 16px;
+    margin: 0 auto;
+  }
+  nav{
+    background-color: #7EA3CC;
+    .holder-navigation{
+      padding: 8px 0px;
+      .link{
+        display: inline-block;
+        color: white;
+        text-transform: uppercase;
+        text-decoration: none;
+        padding: 16px;
+        border-radius: 4px;
+        transition: background-color 0.4s;
+        &:hover{
+          background-color: #255C99;  
+        }
+      }
+    }
+  }
+
+  footer{
+    padding: 32px 0px;
+    background-color: #262626;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    .container{
+      .holder-details{
+        .title{
+          color: white;
+          text-align: center;
+          font-size: 12px;
+          margin-bottom: 6px;
+        }
+        .name{
+          color: white;
+          text-align: center;
+          font-size: 14px;
+        }
+      }
+    }
+  }
+</style>
---
+++ week-1/src/views/Home.vue
@@ -0,0 +1,164 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>Welkom {{name}} bij de cursus!</p>
+    <div id="user-list">
+      <div class="holder-users">
+        <div class="user" v-for="item in deelnemers" :key="item.id">
+          <div class="holder-details">
+            <div class="name">{{ item.naam }}</div>
+            <div class="residence">{{item.afdeling}}</div>
+          </div>
+          <div class="close" @click="deleteUser($event)" v-bind:data-id="item.id"></div>
+        </div>
+      </div>
+      
+      <div class="manual">
+        <div class="title">Wil je iemand toevoegen?</div>
+        <div class="holder-input">
+          <input type="text" v-model="newUser" placeholder="Naam">
+          <input type="text" v-model="newFunction" placeholder="Functie">
+        </div>
+        <div class="button" @click="addUser()">Toevoegen</div>
+      </div>
+      
+      
+      
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+h1{
+  text-align: center;
+}
+p{
+  text-align :center;
+  max-width: 660px;
+  margin: 0 auto;
+  margin-bottom: 24px;
+}
+#user-list{
+  max-width:500px;
+  margin: 0 auto;
+  .holder-users{
+    margin-bottom:24px;
+  }
+  .user{
+    display: flex;
+    align-items: center;
+    padding: 4px 8px;
+    border: solid 2px rgba(0,0,0,0.8);
+    margin-bottom: 4px;
+    
+    .holder-details{
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      width: 100%;
+    }
+    .close{
+      width: 24px;
+      height: 24px;
+      margin-left: 16px;
+      border-radius :4px;
+      background-color: white;
+      border: solid 1px black;
+      background-image: url('https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-close-512.png');
+      background-size: 80%;
+      background-repeat: no-repeat;
+      background-position: center center;
+      cursor: pointer;
+      transition: opacity 0.2s;
+      &:hover{
+        opacity: 0.6;
+      }
+    }
+    
+  }
+  .manual{
+    width: 100%;
+    .title{
+      margin-bottom: 8px;
+    }
+    .holder-input{
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      width: 100%;
+      flex-wrap: wrap;
+      margin-bottom: 16px;
+      input{
+        width: calc( 50% - 56px);
+        padding: 12px 24px;
+        border: solid 1px rgba(0,0,0,0.4);
+        font-size: 16px;
+        border-radius: 4px;
+      }
+    }
+    .button{
+      padding: 6px 12px;
+      display: inline-block;
+      background-color: #255C99;
+      color: white;
+      cursor: pointer;
+      border-radius :3px;
+      transition: opacity 0.4s;
+      &:hover{
+        opacity: 0.8;
+      }
+    }
+  }
+}
+</style>
+
+<script>
+  export default {
+    name: 'Home',
+    data(){
+      return  {
+        name: 'Tom',
+        idCount: 6,
+        newUser: '',
+        newFunction: '',
+        deelnemers: [
+          { id: 1, naam: "Mark van Lit", afdeling: "Platforms" },
+          { id: 2, naam: "Pim Willems", afdeling: "Platforms" },
+          { id: 3, naam: "Tom Luijten", afdeling: "Platforms" },
+          { id: 4, naam: "Jeffrey Happel", afdeling: "Engagement" },
+          { id: 5, naam: "Nick Goossens", afdeling: "Engagement" },
+          { id: 6, naam: "Dennis Leenders", afdeling: "Engagement" },
+        ]
+      }
+    },
+    methods: {
+      addUser(){
+        if(this.newUser && this.newFunction){
+          this.deelnemers.push({
+            id: (this.idCount + 1),
+            naam: this.newUser,
+            afdeling: this.newFunction
+          })
+          this.idCount++
+          this.newUser= '';
+          this.newFunction= '';
+        }
+      },
+
+      deleteUser(event){
+        let id = event.target.dataset.id;
+        for(let i = 0; i < this.deelnemers.length; i++){
+          if(this.deelnemers[i].id == id){
+            this.deelnemers.splice(i, 1)
+          }
+        }
+
+        //NOTE: Om een of andere reden kreeg ik als ik een "let items in this.deelnemers" functie deed geen objecten terug maar index waardes wanneer ik console.log(items) deed.
+        //Ik heb nog niet heel vaak met de nieuwe for loops gewerkt alleen dit "issue" ben ik eerder tegen gekomen. Weet alleen niet hoe ik het beter kan noteren zodat ik als result
+        //een object terug krijg uit de array.
+
+        //Ook heb ik het idee dat het targeten van de "data-id" attribute beter kan dan hoe ik het nu heb gedaan.
+      }
+    }
+  }
+</script>
